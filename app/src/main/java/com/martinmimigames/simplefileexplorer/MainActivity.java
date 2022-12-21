a2d0a34ba403dc7d5760e94a3812b29f87198155
wmartinmimi <martinmimiw@gmail.com>
2022-12-21 21:10:43 +0000

    removed unnecessary import

--- app/src/main/java/com/martinmimigames/simplefileexplorer/MainActivity.java
+++ app/src/main/java/com/martinmimigames/simplefileexplorer/MainActivity.java
@@ -7,7 +7,6 @@ import static android.view.Gravity.CENTER_VERTICAL;
 
 import android.app.Activity;
 import android.app.Dialog;
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
